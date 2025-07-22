b884637b2168ab9cacde77138f46a38e013a04a9
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-07-22 16:34:22 +0200

    milestone3: add style to cards

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -30,29 +30,64 @@ export default function Main(){
                 {actorsData?.map((element) => {
                     return (
 
-                        <div  key={element.id} className=" py-3 col-sm-6 col-md-4 col-lg-3">
-                            <div className="card h-100">
-                                <img src={element.image} className="card-img-top img-fluid" alt={element.name}/>
-                                <div className="card-body">
-                                    <h4 className="card-text text-center">{element.name}</h4>
+                        <div  key={element.id} className=" py-3 col-12">
+
+
+                            <div className="card mb-3">
+                                <div className="row g-0">
+                                    <div className="col-md-4">
+                                    <img src={element.image} className="img-fluid rounded-start w-75 h-100" alt={element.name}/>
+                                    </div>
+                                    <div className="col-md-8 d-flex flex-column justify-content-center align-items-center">
+                                        <div className="card-body">
+                                            <h2 className="card-title pb-3 fs-1 text-center">{element.name}</h2>
+                                            <div className="d-flex flex-column gap-4 fs-5 px-4">
+                                                <span><b>Anno di Nascita:</b> <span>{element.birth_year}</span></span>
+                                                <span><b>Nazionalità:</b> <span>{element.nationality}</span></span>
+                                                <span><b>Biografia: </b><span>{element.biography}</span></span>
+                                                <span><b>Awards: </b><span>{element.awards}</span></span>
+
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
+
                         </div>
                     )
                 })}
+
                 {actressData?.map((element) => {
                     return (
 
-                        <div key={element.id} className=" py-3 col-sm-6 col-md-4 col-lg-3">
-                            <div className="card h-100">
-                                <img src={element.image} className="card-img-top img-fluid" alt={element.name}/>
-                                <div className="card-body">
-                                    <h4 className="card-text text-center">{element.name}</h4>
+                        <div  key={element.id} className=" py-3 col-12">
+
+
+                            <div className="card mb-3">
+                                <div className="row g-0">
+                                    <div className="col-md-4">
+                                    <img src={element.image} className="img-fluid rounded-start w-75 h-100" alt={element.name}/>
+                                    </div>
+                                    <div className="col-md-8 d-flex flex-column justify-content-center align-items-center">
+                                        <div className="card-body">
+                                            <h2 className="card-title pb-3 fs-1 text-center">{element.name}</h2>
+                                            <div className="d-flex flex-column gap-4 fs-5 px-4">
+                                                <span><b>Anno di Nascita:</b> <span>{element.birth_year}</span></span>
+                                                <span><b>Nazionalità:</b> <span>{element.nationality}</span></span>
+                                                <span><b>Biografia: </b><span>{element.biography}</span></span>
+                                                <span><b>Awards: </b><span>{element.awards}</span></span>
+
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
+
                         </div>
                     )
                 })}
+                
+
             </div>
         </div>
     )
